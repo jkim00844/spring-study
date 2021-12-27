a758a913ae3b8bd237bfcd433452eb82d2d8e448
jkim00844 <jkim00844@gmail.com>
2021-12-27 21:02:16 +0900

    #3 서블릿, JSP, MVC 패턴

---
+++ src/main/java/hello/servlet/domain/member/MemberRepository.java
@@ -0,0 +1,40 @@
+package hello.servlet.domain.member;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    private static final MemberRepository instance = new MemberRepository();
+
+    public static MemberRepository getInstance(){
+        return instance;
+    }
+
+    // 싱글톤으로 만들 때는 private 생성자로 아무나 생성하지 못하게 막아야 함.
+    private MemberRepository(){
+    }
+
+    public Member save(Member member){
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    public Member findById(Long id){
+        return store.get(id);
+    }
+
+    public List<Member> findAll(){
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore(){
+        store.clear();
+    }
+}
