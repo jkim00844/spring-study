5fdb75cf5a2c75d23460312d875b2f778e19a7e2
jkim00844 <jkim00844@gmail.com>
2022-03-08 00:46:35 +0900

    7. 의존관계자동주입 - 조회빈이2개이상 - 문제

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -21,8 +21,8 @@ public class AutoAppConfig {
     // 수동 빈 등록이 우선권을 가진다.
     // 하지만 이러한 경우 만들지 말 것 -> 잡기 어려운 애매한 버그
     // 최근 스프링 부트에서는 수동 빈 등록과 자동 빈 등록이 충돌나면 오류가 발생하도록 기본 값을 바꾸었다.
-    @Bean(name = "memoryMemberRepository")
-    public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
-    }
+//    @Bean(name = "memoryMemberRepository")
+//    public MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//    }
 }
