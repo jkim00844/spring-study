e965534e48072a1e2f85c622106fc15575e28b0a
jkim00844 <jkim00844@gmail.com>
2022-03-13 18:56:44 +0900

    9. 빈 스코프 - 프로토타입 스코프 - 싱글톤 빈과 함께 사용시 Provider로 문제 해결

--- core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -1,12 +1,14 @@
 package hello.core.scope;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -35,24 +37,48 @@ public class SingletonWithPrototypeTest1 {
         // 같은 prototypeBean 사용중..
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
 
     }
 
     @Scope("singleton")
     static class ClientBean{
-        private final PrototypeBean prototypeBean;
-        // 생성시점에 주입, ClientBean를 요청하면 프로토타입임에도 불구하고 같은 prototypeBean을 반환한다.
+//        private final PrototypeBean prototypeBean;
+//        // 생성시점에 주입, ClientBean를 요청하면 프로토타입임에도 불구하고 같은 prototypeBean을 반환한다.
+//
+//        @Autowired
+//        public ClientBean(PrototypeBean prototypeBean){
+//            this.prototypeBean = prototypeBean;
+//        }
+//
+//        public int logic(){
+//            prototypeBean.addCount();
+//            return prototypeBean.getCount();
+//        }
 
+        // 방법2 ObjectFactory, ObjectProvider
+//        @Autowired
+//        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
+//
+//        public int logic(){
+//            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
+//            prototypeBean.addCount();
+//            return prototypeBean.getCount();
+//        }
+
+
+        // 방법3 JSR-330 Provider
+        //implementation 'javax.inject:javax.inject:1' gradle 추가 필수
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean){
-            this.prototypeBean = prototypeBean;
+        private Provider<PrototypeBean> provider;
+
+        public int logic() {
+            PrototypeBean prototypeBean = provider.get();
+            prototypeBean.addCount();
+            int count = prototypeBean.getCount();
+            return count;
         }
 
-        public int logic(){
-            prototypeBean.addCount();
-            return prototypeBean.getCount();
-        }
     }
 
     @Scope("prototype")
