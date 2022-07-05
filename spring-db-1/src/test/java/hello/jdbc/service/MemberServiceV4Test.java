ebcd60577f77f1c7873295afefc2187779ef8b77
jkim00844 <jkim00844@gmail.com>
2022-07-06 00:37:08 +0900

    6. 스프링과 문제 해결 - 예외 처리, 반복 - JDBC 반복 문제 해결 - JdbcTemplate

--- spring-db-1/src/test/java/hello/jdbc/service/MemberServiceV4Test.java
+++ spring-db-1/src/test/java/hello/jdbc/service/MemberServiceV4Test.java
@@ -5,9 +5,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import hello.jdbc.domain.Member;
 import hello.jdbc.repository.MemberRepository;
-import hello.jdbc.repository.MemberRepositoryV3;
-import hello.jdbc.repository.MemberRepositoryV4_1;
-import hello.jdbc.repository.MemberRepositoryV4_2;
+import hello.jdbc.repository.MemberRepositoryV5;
 import java.sql.SQLException;
 import javax.sql.DataSource;
 import lombok.extern.slf4j.Slf4j;
@@ -52,7 +50,8 @@ class MemberServiceV4Test {
         @Bean
         MemberRepository memberRepository() {
 //            return new MemberRepositoryV4_1(dataSource);
-            return new MemberRepositoryV4_2(dataSource);
+//            return new MemberRepositoryV4_2(dataSource);
+            return new MemberRepositoryV5(dataSource); //JdbcTemplate
         }
 
         @Bean
